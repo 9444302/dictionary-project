5f21d26a180b5906fbba4644c654495a983f27b6
9444302 <90798635+9444302@users.noreply.github.com>
2021-11-30 16:05:45 -0700

    updated link

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Dictionary</h1>
-      <Dictionary />
-      <footer>
-        <a href="https://github.com/9444302/dictionary-project" target="_blank">
-          Open source code
-        </a>{" "}
-        by Tori
-      </footer>
+        <h1>Dictionary</h1>
+        <Dictionary />
+        <footer>
+          <a
+            href="https://github.com/9444302/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open source code
+          </a>{" "}
+          by Tori
+        </footer>
       </div>
     </div>
   );
