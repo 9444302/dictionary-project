97899bc24b8dafcb17eb031abeebc210bc740406
9444302 <90798635+9444302@users.noreply.github.com>
2021-12-03 12:00:31 -0700

    added synonyms

--- src/Definition.js
+++ src/Definition.js
@@ -1,16 +1,29 @@
 import React from "react";
+import Synonyms from "./Synonyms"
 
 export default function Definition(props) {
-    console.log (props)
-    return(
-        <div>
-    <h3>{props.def.partOfSpeech}</h3>
-    {props.def.definitions.map(function(definition, index) {
-       return(
-       <div key={index}>
-    <p className="def">{`Definition: ${definition.definition}`}</p>
-    <p className="example">{`Example: ${definition.example}`}</p>
+  console.log(props);
+  return (
+    <div>
+      <h3>{props.def.partOfSpeech}</h3>
+      {props.def.definitions.map(function (definition, index) {
+        if (definition.example) {
+          return (
+            <div key={index}>
+              <p className="def">{`Definition: ${definition.definition}`}</p>
+              <p className="example">{`Example: ${definition.example}`}</p>
+              <Synonyms synonyms={definition.synonyms}/>
+            </div>
+          );
+        } else {
+          return (
+              <div key={index}>
+            <p className="def">{`Definition: ${definition.definition}`}</p>
+            <Synonyms synonyms={definition.synonyms}/>
+            </div>
+          );
+        }
+      })}
     </div>
-    )})}
-    </div>
-    )}
+  );
+}
